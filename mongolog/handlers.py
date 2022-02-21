c24d4cd440315ae866e3f176a51a31946a2b60bd
Santiago G. Alegría <arkaikus@outlook.com>
2022-02-21 11:08:01 -0500

    Format file and auto create capped collection

--- mongolog/handlers.py
+++ mongolog/handlers.py
@@ -8,13 +8,12 @@ from datetime import datetime
 from pymongo.collection import Collection
 from socket import gethostname
 
-try:
-    from pymongo import MongoClient as Connection
-except ImportError:
-    from pymongo import Connection
+from pymongo import MongoClient
+
 
 if sys.version_info[0] >= 3:
     unicode = str
+
 
 class MongoFormatter(logging.Formatter):
     def format(self, record):
@@ -31,46 +30,65 @@ class MongoFormatter(logging.Formatter):
             time=datetime.now(),
             host=gethostname(),
             message=msg,
-            args=tuple(unicode(arg) for arg in record.args)
+            args=tuple(unicode(arg) for arg in record.args),
         )
-        if 'exc_info' in data and data['exc_info']:
-            data['exc_info'] = self.formatException(data['exc_info'])
+        if "exc_info" in data and data["exc_info"]:
+            data["exc_info"] = self.formatException(data["exc_info"])
         return data
 
 
 class MongoHandler(logging.Handler):
-    """ Custom log handler
+    """Custom log handler
 
     Logs all messages to a mongo collection. This  handler is
     designed to be used with the standard python logging mechanism.
     """
 
     @classmethod
-    def to(cls, collection, db='mongolog', host='localhost', port=None,
-        username=None, password=None, level=logging.NOTSET):
-        """ Create a handler for a given  """
+    def to(
+        cls,
+        collection,
+        db="mongolog",
+        host="localhost",
+        port=None,
+        username=None,
+        password=None,
+        level=logging.NOTSET,
+    ):
+        """Create a handler for a given"""
         return cls(collection, db, host, port, username, password, level)
 
-    def __init__(self, collection, db='mongolog', host='localhost', port=None,
-        username=None, password=None, level=logging.NOTSET):
-        """ Init log handler and store the collection handle """
+    def __init__(
+        self,
+        db: str,
+        collection: str,
+        host="localhost",
+        port=27017,
+        username=None,
+        password=None,
+        level=logging.NOTSET,
+    ):
+        """Init log handler and store the collection handle"""
         logging.Handler.__init__(self, level)
-        if isinstance(collection, str):
-            connection = Connection(host, port)
-            if username and password:
-                connection[db].authenticate(username, password)
-            self.collection = connection[db][collection]
-        elif isinstance(collection, Collection):
-            self.collection = collection
+        assert isinstance(db, str), "[db] must be a string"
+        assert isinstance(collection, str), "[collection] must be a string"
+
+        connection = MongoClient(host, port)
+        database = connection.get_database(db)
+
+        if username and password:
+            database.authenticate(username, password)
+
+        if collection in database.list_collection_names():
+            self.collection = database[collection]
         else:
-            raise TypeError('collection must be an instance of basestring or '
-                             'Collection')
+            self.collection = database.create_collection(collection, capped=True, size=10000000)
+
         self.formatter = MongoFormatter()
 
     def emit(self, record):
-        """ Store the record to the collection. Async insert """
+        """Store the record to the collection. Async insert"""
         try:
             self.collection.insert_one(self.format(record))
         except InvalidDocument as e:
-            logging.error("Unable to save log record: %s", e.message,
-                exc_info=True)
+            logging.error("Unable to save log record: %s", e.message, exc_info=True)
